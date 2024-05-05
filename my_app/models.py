6af39e33774e431ef02c2331bcd10858f7399c6c
KevinYeff <kevinyeff94@gmail.com>
2024-05-05 15:26:16 -0500

    feat: First model

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    """
+    Project: This class represents a table called project in the database.
+    It stores basic information about a project.
+    
+    Attributes.
+    
+    name (CharField): Field that stores the name of a project in the Project
+    table.
+    """
+    name = models.CharField(max_length=200)
